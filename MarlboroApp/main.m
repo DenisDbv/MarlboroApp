5315ff64f7e5b20edfa3f06c1ea4309c0e37e314
denisdbv <denisdbv@gmail.com>
2014-03-27 09:43:52 +0400

    Initial Commit

---
+++ MarlboroApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MarlboroApp
+//
+//  Created by DenisDbv on 27.03.14.
+//  Copyright (c) 2014 brandmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MRAppDelegate class]));
+    }
+}
